16ed29c555c59eb626b5a40a8057120187ca4717
unknown <abhigyanraushan18@gmail.com>
2024-08-30 09:49:25 +0530

    first commit

---
+++ prog2.py
@@ -0,0 +1,7 @@
+s1=int(input("enter marks of subject 1"))
+s2=int(input("enter marks of subject 2"))
+s3=int(input("enter marks of subject 3"))
+total=s1+s2+s3
+percentage=(total*100)/300
+print("total marks =",total)
+print("percentage = ",percentage)
