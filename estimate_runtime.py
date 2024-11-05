84e5287ec627f5d1425acc8540defc806cdeae84
abhigyanraushan <abhigyanraushan18@gmail.com>
2024-11-05 18:24:14 +0530

    estimate run time

---
+++ estimate_runtime.py
@@ -0,0 +1,18 @@
+#write a program to estimate runtime of all the functions you have done till now
+
+import time as t
+import avgsum.py as sum
+def testfn():
+    pass
+startTime=t.time()
+testfn()
+endTime=t.time()
+runTimeInSec = endTime - startTime
+print(f"\n time to execute function is ", {runTimeInSec/60} min)
+
+
+sum(e)
+
+
+
+# 2 modify all your previous functions to print their execution time
